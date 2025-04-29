8406932aed3d92c64b13af5bc9ae78574fc733e1
chnarimane <iamnarihi@gmail.com>
2025-04-29 23:13:57 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleDetails(button) {
+    const details = button.nextElementSibling;
+    const isVisible = details.style.display === 'block';
+  
+    details.style.display = isVisible ? 'none' : 'block';
+    button.textContent = isVisible ? 'En savoir plus' : 'Réduire';
+  }
+  
